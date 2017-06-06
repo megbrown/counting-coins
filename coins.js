e25f309b01fd9696426d8a5d2d415b3fbcac140f
Megan B <brown.m.j@outlook.com>
2017-06-06 14:53:51 -0500

    added the rest of the instructions

--- coins.js
+++ coins.js
@@ -1,3 +1,9 @@
+// Requirements
+
+// Write a program that will convert a dollar amount to the 
+// number of coins that make up the amount. Use the common 
+// American coins of quarters, dimes, nickels, and pennies.
+
 /*
   Input: 0.67
   Output:
