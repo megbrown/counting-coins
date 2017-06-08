4e98794874e1f7aaed29516f1ee14a3c679e9a6f
Megan B <brown.m.j@outlook.com>
2017-06-08 10:40:03 -0500

    completed exercise

--- coins.js
+++ coins.js
@@ -1,3 +1,5 @@
+console.log("hello megan");
+
 // Requirements
 
 // Write a program that will convert a dollar amount to the 
@@ -13,17 +15,31 @@
     nickels: 1,
     pennies: 2
   }
-
 */
 
-function coinCounter () {
-  // Initialize a JavaScript object to hold the coins
+var input = prompt("Enter a dollar amount");
+console.log("$", input);
+var dollarAmount = input * 100;
+
+function coinCounter (quarters, dimes, nickels, pennies) {
   var coinPurse = {};
   
-  coinPurse.quarters = 0;
+   coinPurse.quarters = Math.floor(dollarAmount / 25);;
+   coinPurse.dimes = Math.floor((dollarAmount % 25) / 10);;
+   coinPurse.nickels = Math.floor(((dollarAmount % 25) % 10) / 5);
+   coinPurse.pennies = Math.floor((((dollarAmount % 25) % 10) % 5) / 1);
 
-  return coinPurse;
-}
+   return coinPurse;
+ }
 
 var coins = coinCounter()
-console.log();
+console.log(coins);
+
+
+
+
+
+
+
+
+
